03f75994d5ba2a002cc66d00d97a3da9994e5b4c
Rahul Kumar <82270398+remotewith@users.noreply.github.com>
2022-10-09 15:51:33 +0530

    Update Webcam access.py

--- Accessing Camera/Webcam access.py
+++ Accessing Camera/Webcam access.py
@@ -1,1 +1,24 @@
+# Importing the required library
+import cv2
 
+# We here make an object named as "video" that has the videocapture method
+# 0 signifies that we are accessing the camera of the local system on which we are working.
+
+video=cv2.VideoCapture(0)
+
+# In this while loop we will run each frame for each loop.
+while True:
+  
+  
+    check,frame=video.read()
+    
+    
+    cv2.imshow("FRAME",frame)
+    
+    
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break
+        
+video.release()
+cv2.destroyAllWindows
