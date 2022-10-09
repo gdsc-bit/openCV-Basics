7e77b2d90ddfddde671abfbffd82c0c4174a30b8
Machupalli Sree Pragna <88078068+PragnaReddy11@users.noreply.github.com>
2022-10-09 13:08:51 +0530

    Update RGBtoGrayScale.py

--- Grayscaling images/RGBtoGrayScale.py
+++ Grayscaling images/RGBtoGrayScale.py
@@ -1,4 +1,11 @@
+#Grayscaling is the process of converting an image from other color spaces e.g. RGB, CMYK, HSV, etc. to shades of gray.
+#Importance of grayscaling 
+#Dimension reduction: For example, In RGB images there are three color channels and three dimensions while grayscale images are single-dimensional.
+#Reduces model complexity: Consider training neural articles on RGB images of 10x10x3 pixels. The input layer will have 300 input nodes. On the other hand, the same neural network will need only 100 input nodes for grayscale images.
+#For other algorithms to work: Many algorithms are customized to work only on grayscale images e.g. Canny edge detection function pre-implemented in the OpenCV library works on Grayscale images only.
 
+<h2>Let’s learn the different image processing methods to convert a colored image into a grayscale image.</h2>
+<h2>Method 1: Using the cv2.cvtColor() function</h2>
 import cv2
 image = cv2.imread('C:\\Documents\\full_path\\tomatoes.jpg')
 cv2.imshow('Original', image)
