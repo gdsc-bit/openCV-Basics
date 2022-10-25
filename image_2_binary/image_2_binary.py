bc21dfdae497a7f23d1efb6633fa9d6787667e8f
HARSH ADHIKARI <harshadhikari023@gmail.com>
2022-10-25 18:54:26 +0530

    Update image_2_binary.py

--- image_2_binary/image_2_binary.py
+++ image_2_binary/image_2_binary.py
@@ -1,10 +1,9 @@
-from PIL import Image 
-input_image = Image.open()     #path to your image
-pixel_map = input_image.load()
-width, height = input_image.size
+import cv2
+input_image = cv2.imread()     #path to your image
+width, height, channels = input_image.shape
 for i in range(width):
   for j in range(height):
-    r,g,b = input_image.get.pixel((i,j))
+    r,g,b = input_image[i,j]
     lumino = ((0.299*r + 0.587*g + 0.114*b)/255)
     if(lumino < 0.5):
       a = 0
